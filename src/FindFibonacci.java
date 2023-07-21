7a2abd1db3a0e802aeb7c84d2bf72a8b04dd95f4
Nirikshamanoj <niriksha.m1709@gmail.com>
2023-07-21 11:54:48 +0530

    First commit for Find Fibonacci

---
+++ src/FindFibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FindFibonacci {
+    static int findFibonacci(int n)
+    {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        return (findFibonacci(n-1)+findFibonacci(n-2));
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n= scanner.nextInt();
+        int fibonacci=findFibonacci(n);
+        System.out.println(fibonacci);
+    }
+}
